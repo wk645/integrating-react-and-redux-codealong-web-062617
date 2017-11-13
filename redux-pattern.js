b81a8a36fca9f2b73f6653218b3a8022de91f2cd
wk645 <wk645@nyu.edu>
2017-11-13 17:23:57 -0500

    integrate react with redux

--- redux-pattern.js
+++ redux-pattern.js
@@ -1,33 +1,33 @@
-function createStore(reducer) {
-  let state;
+// function createStore(reducer) {
+//   let state;
 
-  function dispatch(action) {
-    state = reducer(state, action);
-    console.log(`the state is ${state.count}`);
-    console.log(`the action is ${action.type}`);
-    render();
-  };
+//   function dispatch(action) {
+//     state = reducer(state, action);
+//     console.log(`the state is ${state.count}`);
+//     console.log(`the action is ${action.type}`);
+//     render();
+//   };
 
-  function getState(){
-    return state;
-  };
+//   function getState(){
+//     return state;
+//   };
 
-  return {
-    dispatch,
-    getState
-  };
-};
+//   return {
+//     dispatch,
+//     getState
+//   };
+// };
 
-function changeCount(state = {
-  count: 0,
-}, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
-    default:
-      return state;
-  };
-};
+// function changeCount(state = {
+//   count: 0,
+// }, action) {
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return { count: state.count + 1 };
+//     default:
+//       return state;
+//   };
+// };
 
 
 function render(){
